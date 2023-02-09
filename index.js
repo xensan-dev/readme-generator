3d93f9c70a326bdf912bcffd93b0cbe8b45da888
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 18:03:17 -0800

    deleted node_modules folder from repo and included .gitignore.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -56,19 +56,19 @@ const questions = [
     }
 ];
 
-//prompt user function
+// prompt user function
 const promptUser = function(){
     return inquirer.prompt(questions)
 } 
 
-// TODO: Create a function to write README file
+// function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(`${fileName}`, generateMarkdown(data), function(err){
         if (err) return console.log(err)
     })
 }
 
-// TODO: Create a function to initialize app
+// function to initialize app
 function init() {
     promptUser()
         .then((answers) => writeToFile('README.md', answers))
